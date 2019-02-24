df511249d4de7f64c5ad230abf680cd5a6bd9014
pranjalpruthi <pranjalpruthi@gmail.com>
2019-02-24 19:15:04 +0530

    re2

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; Molecular Probes DB &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/https://pranjalpruthi.github.io/rtpcr/wp-includes/css/dashicons.min.css?ver=5.1' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/https://pranjalpruthi.github.io/rtpcr/wp-includes/css/buttons.min.css?ver=5.1' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/https://pranjalpruthi.github.io/rtpcr/wp-admin/css/forms.min.css?ver=5.1' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/https://pranjalpruthi.github.io/rtpcr/wp-admin/css/l10n.min.css?ver=5.1' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='/https://pranjalpruthi.github.io/rtpcr/wp-admin/css/login.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://localhost/https:/pranjalpruthi.github.io/rtpcr/wp-includes/css/dashicons.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='http://localhost/https:/pranjalpruthi.github.io/rtpcr/wp-includes/css/buttons.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='http://localhost/https:/pranjalpruthi.github.io/rtpcr/wp-admin/css/forms.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='http://localhost/https:/pranjalpruthi.github.io/rtpcr/wp-admin/css/l10n.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://localhost/https:/pranjalpruthi.github.io/rtpcr/wp-admin/css/login.min.css?ver=5.1' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -22,7 +22,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Powered by WordPress">Powered by WordPress</a></h1>
 	
-	<form name="loginform" id="loginform" action="/https://pranjalpruthi.github.io/rtpcr/wp-login.php" method="post">
+	<form name="loginform" id="loginform" action="http://localhost/https:/pranjalpruthi.github.io/rtpcr/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
@@ -40,7 +40,7 @@
 	</form>
 
 			<p id="nav">
-					<a href="/https://pranjalpruthi.github.io/rtpcr/wp-login.php?action=lostpassword">Lost your password?</a>
+					<a href="http://localhost/https:/pranjalpruthi.github.io/rtpcr/wp-login.php?action=lostpassword">Lost your password?</a>
 				</p>
 	
 	<script type="text/javascript">
@@ -57,7 +57,7 @@
 			if(typeof wpOnload=='function')wpOnload();
 			</script>
 
-			<p id="backtoblog"><a href="/https://pranjalpruthi.github.io/rtpcr/">
+			<p id="backtoblog"><a href="http://localhost/https:/pranjalpruthi.github.io/rtpcr/">
 		&larr; Back to Molecular Probes DB	</a></p>
 			
 	</div>
